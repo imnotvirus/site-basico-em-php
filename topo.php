d1b2de1bdb6a1043ba8a9560c2853c8e8b816234
imnotvirus <lcluizclaudio96@gmail.com>
2015-04-16 23:28:14 -0300

    Create topo.php

---
+++ topo.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title> school of net</title>
+	</head>
+	
+	<body>
+		
+		<div id="estrutura">
+			<div id="topo">
+				<h1>school of Net - site a jato</h1>
+				<div id="menu">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="empresa.php">Empresa</a></li>
+						<li><a href="produtos.php">Produtos</a></li>
+						<li><a href="contato.php">Contato</a></li>
+					</ul>
+				</div>
+			</div>
+	
+			<hr />
+
